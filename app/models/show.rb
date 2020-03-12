87452b9cb1af19d799fffe77fedadc960395185d
dwane richards <dwanerich@gmail.com>
2020-03-12 01:08:31 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,34 +1,5 @@
 class Show < ActiveRecord::Base
   
-#   def self.highest_rating
-#   Show.maximum(:rating)
-#   end
-  
-#   def self.most_popular_show
-#     self.where("rating = ?", maximum)
-#   end
-  
-#   def self.lowest_rating
-#     Show.minimum(:rating)
-#   end
-  
-#   def self.least_popular_show
-#     self.where("rating = ?", 0)
-#   end
-  
-#   def self.ratings_sum
-#     self.sum(:rating)
-#   end
-  
-#   def self.popular_shows
-#     self.where("rating > ?", 5)
-#   end
-  
-#   def self.shows_by_alphabetical_order
-#     # Show.all.sort
-#   end
-# end
-
 def Show::highest_rating
     Show.maximum("rating")
   end
