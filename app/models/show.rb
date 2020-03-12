ba6ea7fbbebbedfd5608e5a73ac33d0ccbc1ccc1
dwane richards <dwanerich@gmail.com>
2020-03-12 00:58:47 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -25,6 +25,8 @@ class Show < ActiveRecord::Base
   end
   
   def self.shows_by_alphabetical_order
-    Show.all.sort
+    # Show.all.sort
+    change = Show.all
+change.sort {|anythinghere| a <=> b puts "Ascending #{anythinghere}" }
   end
 end
