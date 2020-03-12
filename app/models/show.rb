dd16268b3be979c8a4869bb67b69a9bf02b07ef7
dwane richards <dwanerich@gmail.com>
2020-03-12 00:49:53 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -21,7 +21,7 @@ class Show < ActiveRecord::Base
   end
   
   def self.popular_shows
-    shows.where("rating > ?", 5)
+    where("rating > ?", 5)
   end
   
   def self.shows_by_alphabetical_order
